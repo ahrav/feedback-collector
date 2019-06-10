ec553bf791f746fb226b40d0cefeae923871797d
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-10 07:57:18 -0700

    handle front end authentication

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Header = () => {
+const Header = ({ auth }) => {
+  const renderContent = () => {
+    switch (auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href='/auth/google'>Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href='/api/logout'>Logout</a>
+          </li>
+        );
+    }
+  };
+
   return (
     <nav>
       <div className='nav-wrapper'>
         <a className='left brand-logo'>Feedback Collector</a>
-        <ul className='right'>
-          <li>
-            <a>Login with Google</a>
-          </li>
-        </ul>
+        <ul className='right'>{renderContent()}</ul>
       </div>
     </nav>
   );
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Header);
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,12 +1,17 @@
 import { FETCH_USER } from '../actions/types';
 
-const initialState = {};
+// const initialState = {
+//   user: null,
+//   loading: true,
+//   isAuthenticated: null
+// };
 
-export default (state = initialState, action) => {
-  console.log(action);
+export default (state = null, action) => {
   const { type, payload } = action;
 
   switch (type) {
+    case FETCH_USER:
+      return payload || false;
     default:
       return state;
   }
