db87f2d38e3b9addca395319ec92b981c2c10302
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-10 03:44:41 -0700

    add keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,7 +3,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./dev');
 }
-
-// mongodb+srv://ahrav:V7XSMNpRkarQM1jA@cluster0-8krrb.mongodb.net/feedback-collector-prod?retryWrites=true&w=majority
-// "client_id":"964137436532-tr5l0kbem4j7oinr241ccrmtbq1dd0hh.apps.googleusercontent.com"
-// "client_secret":"bA3m142r0nJ5CzZWn1ghzsgs"
