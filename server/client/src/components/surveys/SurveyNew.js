5bc1b59ec6e8529140d0d7acd03b7f03b810b2a8
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-11 02:51:49 -0700

    get redux-from incorporated to frontend

---
+++ server/client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const SurveyNew = props => {
+  return <div>Survey New</div>;
+};
+
+SurveyNew.propTypes = {};
+
+export default connect(
+  null,
+  null
+)(SurveyNew);
